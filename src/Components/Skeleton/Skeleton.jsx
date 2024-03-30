f1d528743c59d087370e3aa7d5e5e1865fb1b51e
Saad Khan <72255519+saadd28@users.noreply.github.com>
2024-03-31 03:12:32 +0500

    Initialize project using Create React App

---
+++ src/Components/Skeleton/Skeleton.jsx
@@ -0,0 +1,13 @@
+import { BG } from "../../Assets"
+import "./Skeleton.css"
+
+import React from 'react'
+
+export default function Skeleton() {
+  return (
+    <>
+    <img src={BG} alt="BG" className="bg_img"/>
+    
+    </>
+  )
+}
